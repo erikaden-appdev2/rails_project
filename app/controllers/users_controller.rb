a4aa6505ec285eb4630040d4c56ed460af0e942b
erikadenedu <eaden@chicagobooth.edu>
2021-06-02 14:54:24 +0000

    user_show_rcav

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show liked feed followers following discover ]
+  before_action :set_user, only: %i[ show ]
 
   private
 
